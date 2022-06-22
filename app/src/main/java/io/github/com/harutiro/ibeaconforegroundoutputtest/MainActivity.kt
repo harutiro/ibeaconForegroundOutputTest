04ac68be0afcf337724f95c86616008cc27437a9
harutiro <harutiro2027@outlook.jp>
2022-06-22 22:21:14 +0900

    update: パーミッションの設定。

--- app/src/main/java/io/github/com/harutiro/ibeaconforegroundoutputtest/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/ibeaconforegroundoutputtest/MainActivity.kt
@@ -1,16 +1,52 @@
 package io.github.com.harutiro.ibeaconforegroundoutputtest
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.com.harutiro.ibeaconforegroundoutputtest.databinding.ActivityMainBinding
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity() {
 
+    //bindingのインスタンス化
     private lateinit var binding: ActivityMainBinding
+
+    //intent用のID指定。Intentから戻ってくる時に使うとかなんとか。
+    private val PERMISSION_REQUEST_CODE = 1
+
+
+    //許可して欲しいパーミッションの記載、
+    //Android１２以上ではBlueToothの新しいパーミッションを追加する。
+    val permissions = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_ADVERTISE
+
+        )
+    }else{
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //パーミッション確認
+        if (!EasyPermissions.hasPermissions(this, *permissions)) {
+            // パーミッションが許可されていない時の処理
+            EasyPermissions.requestPermissions(this, "パーミッションに関する説明", PERMISSION_REQUEST_CODE, *permissions)
+        }
+
+
+
     }
 }
